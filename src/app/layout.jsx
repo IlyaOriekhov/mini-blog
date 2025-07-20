6b8003af972e8ed20938855ed0c5274668669d53
Illia <ilya.orekhov@ukr.net>
2025-07-20 18:58:06 +0300

    fix layouts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,17 @@
 import "./globals.css";
 
+export const metadata = {
+  title: {
+    template: "%s | Mini-Blog",
+    default: "Mini-Blog",
+  },
+  description: "A small multilingual blog created with Next.js.",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/icon.svg" type="image/svg+xml" sizes="any" />
       <body suppressHydrationWarning={true}>{children}</body>
     </html>
   );
